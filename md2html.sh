35dfdc825218f44cc37cbae75e9713199dbd2e24
bmacho <18089594+bmacho@users.noreply.github.com>
2021-09-13 18:40:51 +0200

    no need for md2html, faster yarn dev

--- md2html.sh
+++ md2html.sh
@@ -1,35 +1,35 @@
 #!/bin/sh
 
-# Learn docs
-cd static/docs
-
-export PATH="../../node_modules/.bin/:$PATH"
-
-for f in *.md; do
-showdown makehtml -i "$f" -o "$(basename -- "$f" .md).html" --flavor github
-done
-
-for f in es/*.md; do
-showdown makehtml -i "$f" -o "$(basename -- "$f" .md).es.html" --flavor github
-done
-
-for f in hu/*.md; do
-showdown makehtml -i "$f" -o "$(basename -- "$f" .md).hu.html" --flavor github
-done
-
-for f in it/*.md; do
-showdown makehtml -i "$f" -o "$(basename -- "$f" .md).it.html" --flavor github
-done
-
-for f in pt/*.md; do
-showdown makehtml -i "$f" -o "$(basename -- "$f" .md).pt.html" --flavor github
-done
-
-for f in fr/*.md; do
-showdown makehtml -i "$f" -o "$(basename -- "$f" .md).fr.html" --flavor github
-done
-
-showdown makehtml -i "zh/intro.md" -o "intro.zh.html" --flavor github
+#~ # Learn docs
+#~ cd static/docs
+#~
+#~ export PATH="../../node_modules/.bin/:$PATH"
+#~
+#~ for f in *.md; do
+#~ showdown makehtml -i "$f" -o "$(basename -- "$f" .md).html" --flavor github
+#~ done
+#~ 
+#~ for f in es/*.md; do
+#~ showdown makehtml -i "$f" -o "$(basename -- "$f" .md).es.html" --flavor github
+#~ done
+#~
+#~ for f in hu/*.md; do
+#~ showdown makehtml -i "$f" -o "$(basename -- "$f" .md).hu.html" --flavor github
+#~ done
+#~
+#~ for f in it/*.md; do
+#~ showdown makehtml -i "$f" -o "$(basename -- "$f" .md).it.html" --flavor github
+#~ done
+#~
+#~ for f in pt/*.md; do
+#~ showdown makehtml -i "$f" -o "$(basename -- "$f" .md).pt.html" --flavor github
+#~ done
+#~
+#~ for f in fr/*.md; do
+#~ showdown makehtml -i "$f" -o "$(basename -- "$f" .md).fr.html" --flavor github
+#~ done
+#~
+#~ showdown makehtml -i "zh/intro.md" -o "intro.zh.html" --flavor github
 
 
 SRC='https://github.com/gbtami/pychess-variants/blob/master'; 
